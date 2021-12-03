9a33551e2a15dbba057c4c3a078a7f3f04b51580
JohnPaul-Londono <johnplondono@gmail.com>
2021-12-03 15:28:25 -0800

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using dojo_survery_model.Models;
+
+namespace dojo_survery_model.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        static  Survey oneSurvey;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost("process")]
+        public IActionResult Process(Survey newSurvey)
+        {
+            oneSurvey = newSurvey;
+            return RedirectToAction("Result");
+        }
+
+        [HttpGet("Result")]
+        public IActionResult Result()
+        {
+            return View(oneSurvey);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace dojo_survery_model.Models
+{
+    
+    public class Survey
+    {
+        public string Name{get;set;}
+        public string Location{get;set;}
+        public string Language{get;set;}
+        public string Comments{get;set;}
+    }
+}
